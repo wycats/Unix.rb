3e1e7f33ab5cc06231c2f98c6e0d9466d36cc5ba
Yehuda Katz <wycats@gmail.com>
2011-08-06 19:48:50 -0700

    Generate the macro definitions from the list provided by SUS3

--- ext/c_sysconf_macros.c
+++ ext/c_sysconf_macros.c
@@ -4,40 +4,7 @@ static struct {
 	const char *name;
 	intptr_t value;
 } sysconf_names[] = {
-	{ "ARG_MAX",  					(intptr_t)_SC_ARG_MAX },
-	{ "CHILD_MAX", 					(intptr_t)_SC_CHILD_MAX },
-	{ "CLK_TCK",  					(intptr_t)_SC_CLK_TCK },
-	{ "IOV_MAX",     				(intptr_t)_SC_IOV_MAX },
-	{ "NGROUPS_MAX", 				(intptr_t)_SC_NGROUPS_MAX },
-	{ "NPROCESSORS_CONF", 	(intptr_t)_SC_NPROCESSORS_CONF },
-	{ "NPROCESSORS_ONLN", 	(intptr_t)_SC_NPROCESSORS_ONLN },
-	{ "OPEN_MAX", 					(intptr_t)_SC_OPEN_MAX },
-	{ "PAGESIZE", 					(intptr_t)_SC_PAGESIZE },
-	{ "STREAM_MAX", 				(intptr_t)_SC_STREAM_MAX },
-	{ "TZNAME_MAX", 				(intptr_t)_SC_TZNAME_MAX },
-	{ "JOB_CONTROL", 				(intptr_t)_SC_JOB_CONTROL },
-	{ "SAVED_IDS", 					(intptr_t)_SC_SAVED_IDS },
-	{ "VERSION", 						(intptr_t)_SC_VERSION },
-	{ "BC_BASE_MAX", 				(intptr_t)_SC_BC_BASE_MAX },
-	{ "BC_DIM_MAX", 				(intptr_t)_SC_BC_DIM_MAX },
-	{ "BC_SCALE_MAX",   		(intptr_t)_SC_BC_SCALE_MAX },
-  { "BC_STRING_MAX", 			(intptr_t)_SC_BC_STRING_MAX },
-	{ "COLL_WEIGHTS_MAX", 	(intptr_t)_SC_COLL_WEIGHTS_MAX },
-	{ "EXPR_NEST_MAX",  		(intptr_t)_SC_EXPR_NEST_MAX },
-	{ "LINE_MAX", 					(intptr_t)_SC_LINE_MAX },
-	{ "RE_DUP_MAX", 				(intptr_t)_SC_RE_DUP_MAX },
-	{ "2_VERSION", 					(intptr_t)_SC_2_VERSION },
-	{ "2_C_BIND", 					(intptr_t)_SC_2_C_BIND },
-	{ "2_C_DEV", 						(intptr_t)_SC_2_C_DEV },
-	{ "2_CHAR_TERM",   			(intptr_t)_SC_2_CHAR_TERM },
-	{ "2_FORT_DEV", 				(intptr_t)_SC_2_FORT_DEV },
-	{ "2_FORT_RUN", 				(intptr_t)_SC_2_FORT_RUN },
-	{ "2_LOCALEDEF", 				(intptr_t)_SC_2_LOCALEDEF },
-	{ "2_SW_DEV", 					(intptr_t)_SC_2_SW_DEV },
-	{ "2_UPE", 							(intptr_t)_SC_2_UPE },
-#ifdef _SC_PHYS_PAGES
-	{ "PHYS_PAGES", 				_SC_PHYS_PAGES },
-#endif
+#include "gen/sysconf.inc"
 	{ NULL, 0 }
 };
 
@@ -48,10 +15,17 @@ void init_c_sysconf_macros(VALUE unix_module) {
 	VALUE value;
 
 	int index;
+	intptr_t raw_value;
 
 	for (index = 0; sysconf_names[index].name != NULL; index++) {
 		name = ID2SYM(rb_intern((sysconf_names[index].name)));
-		value = INT2NUM(sysconf_names[index].value);
+		raw_value = sysconf_names[index].value;
+
+		if (raw_value == 0) {
+			value = ID2SYM(rb_intern("undefined"));
+		} else {
+			value = INT2NUM(raw_value);
+		}
 
 		rb_hash_aset(symbol_hash, name, value);
 		rb_hash_aset(int_hash, value, name);
--- ext/c_unix_sysconf.c
+++ ext/c_unix_sysconf.c
@@ -1,5 +1,32 @@
 #include "c_unix_sysconf.h"
 
+/*
+ * call-seq:
+ *    Unix.sysconf(option)     -> Fixnum
+ *
+ * This method allows you to get the value of a POSIX configuration option 
+ *
+ * +option+ can be a Symbol or a Fixnum. If option is a Symbol, it must be
+ * one of the options defined in SUS3 without the leading +_SC_+.
+ *
+ * Example:
+ *
+ *    Unix.sysconf(:PROCESSORS_CONF) -> 2
+ *
+ * You can get a list of available symbols by inspecting the keys in
+ * +Unix::SYSCONF_NAMES+  That constant is populated at compile time
+ * from the SUS3 values.
+ *
+ * If you know the integer value of a particular option for your system
+ * that is not defined by POSIX.1, you can pass it in directly. If your
+ * system does not recognize the number, +Unix.sysconf+ will raise
+ * a +Errno::EINVAL+.
+ *
+ * If the call results in another value of errno being set, the
+ * corresponding +Errno+ will be raised. If you pass in a Symbol
+ * not found in +Unix::SYSCONF_NAMES+, an +Errno::EINVAL+ will be
+ * raised.
+ */
 static VALUE rb_sysconf(VALUE self, VALUE symbolic_name) {
   VALUE rb_mUnix = rb_const_get(rb_cObject, rb_intern("Unix"));
   VALUE unix_macros = rb_const_get(rb_mUnix, rb_intern("SYSCONF_NAMES"));
@@ -14,25 +41,25 @@ static VALUE rb_sysconf(VALUE self, VALUE symbolic_name) {
 	if (TYPE(symbolic_name) == T_SYMBOL) {
     int_name = rb_hash_aref(unix_macros, symbolic_name);
     if (int_name == Qnil) {
-      rb_raise(rb_const_get(rb_mErrno, rb_intern("EINVAL")), "Invalid sysconf name: %s", rb_id2name(SYM2ID(symbolic_name)));
+      rb_raise(rb_const_get(rb_mErrno, rb_intern("EINVAL")),
+          "Invalid sysconf name: %s",
+          rb_id2name(SYM2ID(symbolic_name)));
     }
 	} else if (TYPE(symbolic_name) == T_FIXNUM) {
     int_name = symbolic_name;
   } else {
-    rb_raise(rb_eArgError, "Invalid argument: %s. Expected Symbol or Integer, got %s",
+    rb_raise(rb_eArgError, "Invalid argument: %s. Expected Symbol or Fixnum, got %s",
         RSTRING_PTR(rb_inspect(symbolic_name)),
         rb_obj_classname(symbolic_name));
   }
 
   name = NUM2INT(int_name);
 
-	if ((value = sysconf(name)) == -1) {
-    if (errno == 0) {
-      return INT2NUM(value);
-    } else {
-      VALUE error_symbol = rb_hash_aref(unix_errnos, INT2NUM(errno));
-      rb_sys_fail("sysconf");
-    }
+	// if errno is zero, a sysconf value of -1 might not be an error. For example,
+	// a -1 return for _SC_ATEXIT_MAX means "infinite"
+	if ((value = sysconf(name)) == -1 && errno != 0) {
+    VALUE error_symbol = rb_hash_aref(unix_errnos, INT2NUM(errno));
+    rb_sys_fail("sysconf");
   } else {
     return INT2NUM(value);
   }
